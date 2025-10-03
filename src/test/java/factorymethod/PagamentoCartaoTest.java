6f9f70c6630183dc3fe4b41743f97ccf71b39fd7
devjuliomartins <contatoeujuliomartins@gmail.com>
2025-10-03 11:54:32 -0300

    Initial commit

--- src/test/java/factorymethod/PagamentoCartaoTest.java
+++ src/test/java/factorymethod/PagamentoCartaoTest.java
@@ -1,4 +1,20 @@
 package factorymethod;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PagamentoCartaoTest {
+
+    @Test
+    void devePagarCartao() {
+        IPagamento pagamento = PagamentoFactory.efetuarPagamento("Cartao");
+        assertEquals("Pagamento via Cartão efetivado", pagamento.pagar());
+    }
+
+    @Test
+    void deveAgendarCartao() {
+        IPagamento pagamento = PagamentoFactory.efetuarPagamento("Cartao");
+        assertEquals("Pagamento via Cartão agendada", pagamento.agendar());
+    }
 }
